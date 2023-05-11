396c2a053e8eeb5c8c45f1e7a61fc67c15d073a5
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-05-11 16:21:24 -0600

    Part 3 implemented

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import potentialCountriesReducer from "./slices/potentialCountriesSlice";
 
 export default configureStore({
-    reducer: {},
-})
+  reducer: { potentialCountries: potentialCountriesReducer },
+});
+
